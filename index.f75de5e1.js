a1f1475c097256379fecbdd660bc2be1dd9c4a60
Marian <getmoodyup@gmail.com>
2024-07-08 19:54:54 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,36 @@
+"use strict";
+const promise1 = new Promise((resolve, reject)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", function() {
+        resolve("Promise was resolved!");
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject("Promise was rejected");
+    }, 3000);
+});
+promise1.then((message)=>{
+    const div = document.createElement("div");
+    div.className = "message";
+    div.textContent = message;
+    document.body.appendChild(div);
+}).catch((err)=>{
+    const div = document.createElement("div");
+    div.className = "message error-message";
+    div.textContent = err;
+    document.body.appendChild(div);
+});
+promise2.then((message)=>{
+    const div = document.createElement("div");
+    div.className = "message";
+    div.textContent = message;
+    document.body.appendChild(div);
+}).catch((err)=>{
+    const div = document.createElement("div");
+    div.className = "message error-message";
+    div.textContent = err;
+    document.body.appendChild(div);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
